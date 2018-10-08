732f9b13c267c39251d111889bb4a8309f360660
avinash karra <avkarra@gmail.com>
2018-10-08 09:59:22 -0400

    Working App

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+// Import React
+import React from 'react';
+
+// Import UI components from reactstrap framework.
+import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+
+// Import the Container component. 
+import Container from "../../components/Container";
+
+// Import css
+import './Footer.css';
+
+// Footer component
+const Footer = (props) => {
+    return (
+        <div className="footer">
+            <Container >
+                <Card>
+                    <CardBody>
+                        <CardTitle className="footer-heading">
+                            New York Times React Search
+                        </CardTitle>
+                        
+                        <a href="https://github.com/aviboot92/Nyt-React-Search" target="_blank" rel="noopener noreferrer" className="footer-button">
+                            <Button size="lg">GitHub Repo</Button>
+                        </a>
+                    </CardBody>
+                </Card>
+            </Container>
+        </div>
+    );
+};
+
+// export Footer component.
+export default Footer;
